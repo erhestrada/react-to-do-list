36dfe9f0ce33e5dcd4b642a15686b01eef2cad0c
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-24 23:26:09 -0400

    added edit icons

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -6,7 +6,9 @@ export default function ToDoList() {
         <ul className="ToDoList">
             {toDoList.map((element, index) => {
                 return(
-                    <li className="toDoItem" key={index}>{element}</li>
+                    <li className="toDoItem" key={index}>{element}
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    </li>
                 )
             })}
 
